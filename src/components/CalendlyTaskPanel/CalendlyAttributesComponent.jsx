c45b2b2c0bf9443d0188840d2bace646b17abe08
seluwa-twilio <seluwa@twilio.com>
2022-08-10 15:05:51 +0100

    updated to fix flex attributes issue

--- src/components/CalendlyTaskPanel/CalendlyAttributesComponent.jsx
+++ src/components/CalendlyTaskPanel/CalendlyAttributesComponent.jsx
@@ -12,23 +12,24 @@ const CalendlyAttributesComponent = (props) => {
   return (
     <Theme.Provider theme="default">
          <div>
-            <br />
-            <Heading as='h3' variant='heading30'>Additional task information</Heading>
-            <ul>
-              <li>Customer Name - {props.task.attributes.callerName}</li>
-              <li>Customer Email - {props.task.attributes.callerEmail}</li>
-              <li>Event Uri - {props.task.attributes.eventUrl}</li>
-              <li>Questions</li>
-              {Object.keys(props.task.attributes.questions).map((key, index) => (
-                  <li>Question {key}: {JSON.stringify(props.task.attributes.questions[key].question)} Answer: {JSON.stringify(props.task.attributes.questions[key].answer)}</li>
-                ))}
-             </ul>
+          {props.task.attributes.type === "calendly" ? 
+            <div>            <br />
+              <Heading as='h3' variant='heading30'>Additional task information</Heading>
+              <ul>
+                <li>Customer Name - {props.task.attributes.callerName}</li>
+                <li>Customer Email - {props.task.attributes.callerEmail}</li>
+                <li>Questions</li>
+                {Object.keys(props.task.attributes.questions).map((key, index) => (
+                    <li>Question {key}: {JSON.stringify(props.task.attributes.questions[key].question)} Answer: {JSON.stringify(props.task.attributes.questions[key].answer)}</li>
+                  ))}
+              </ul>
+             </div>:<></>
+
+          }
          </div>
     </Theme.Provider>
   );
 
 }
-
-
 
 export default withTaskContext(CalendlyAttributesComponent);
